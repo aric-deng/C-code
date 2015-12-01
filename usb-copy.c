fe183ae360729aac85ac922d44a9bf9c102d5cb1
aricdhs@qq.com <aricdhs@qq.com>
2015-12-01 14:38:55 +0800

    add waitpid to get usb-copy process status

--- usb-copy.c
+++ usb-copy.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
 
-static int cPid=0;
+static int iPid=0;
 int main(int argc, char* argv[])
 {
 	if(argc != 3 || argv[1] == NULL || argv[2] == NULL) {
@@ -22,7 +25,7 @@ int main(int argc, char* argv[])
             printf("My pid = %d,parentpid = %d\n",getpid(),getppid());
 			
 			//save current pid 
-			cPid = getpid();  
+			iPid = getpid();  
 			printf("dhs copying from %s to %s \n", argv[1], argv[2]);
 			execl("/bin/cp", "cp", "-rf", argv[1], argv[2], NULL );
 
@@ -31,7 +34,20 @@ int main(int argc, char* argv[])
 			
         default:
             printf("Parent process is running\n");
-            break;
+			sleep(2);
+			int status;
+		
+			while (waitpid(pid,&status,0) < 0);
+			
+			if (WIFEXITED(status)) {
+			   printf("exited, status=%d\n", WEXITSTATUS(status));
+			} else if (WIFSIGNALED(status)) {
+			   printf("killed by signal %d\n", WTERMSIG(status));
+			} else if (WIFSTOPPED(status)) {
+			   printf("stopped by signal %d\n", WSTOPSIG(status));
+			}
+		  
+			break;
     }
 	
 	printf("dhs test out");
